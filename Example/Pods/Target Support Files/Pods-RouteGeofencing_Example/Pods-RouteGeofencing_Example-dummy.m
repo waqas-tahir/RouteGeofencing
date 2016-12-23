5947e83c9a7931cb2453bb16902dd09fa77cc09b
Waqas Tahir <waqastahir@Waqass-MacBook-Air.local>
2016-12-23 17:56:56 +0500

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-RouteGeofencing_Example/Pods-RouteGeofencing_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RouteGeofencing_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RouteGeofencing_Example
+@end
---
+++ Example/Pods/Target Support Files/RouteGeofencing/RouteGeofencing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RouteGeofencing : NSObject
+@end
+@implementation PodsDummy_RouteGeofencing
+@end
